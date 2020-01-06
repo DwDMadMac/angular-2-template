ca16ecdba8f169e3d8a91c5eb632d28e481e60eb
DwDMadMac <anthonymmacallister@gmail.com>
2020-01-06 17:27:12 -0500

    basic site with video selection

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,8 +1,22 @@
 import {Component} from 'angular2/core';
+import {Config} from './config.service';
+import {Video} from './video';
+import {PlaylistComponent} from "./playlist.component";
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Angular 2 Template</h1>'
+    templateUrl: 'app/ts/app.component.html',
+    directives: [PlaylistComponent]
 })
 
-export class AppComponent {}
+export class AppComponent {
+    mainHeading = Config.MAIN_HEADING;
+    videos:Array<Video>;
+
+    constructor() {
+        this.videos = [
+            new Video(1, "VR6 First Drive", "vvxwEgFugvU&t", "First drive after VR6 rebuild"),
+            new Video(2, "VR6 Big Turboe", "IoBljFoszC0", "750WHP+ First Drive")
+        ]
+    }
+}
